67e3922452a0d7aac4e46e9c2a09736026fa8358
BenCmjn <benjamin.calmejane@gmail.com>
2018-01-18 20:26:01 +0100

    final touch and commentaires

--- scrap_crypto.rb
+++ scrap_crypto.rb
@@ -2,16 +2,18 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
-
+#Méthode pour récupérer les noms des cryptos :
 def get_btc_course()
 	page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
 	
-	prices = []
+	prices = [] # On va remplir un tableau vide
 
 	page.xpath('//a[@class="currency-name-container"]').each do |price|
-		prices << price.text #.gsub(' ','')
+		prices << price.text
 
-		#prices[0].gsub("\n","")
+		# .gsub() => au cas où on a des espaces relous
+		# prices[0].gsub("\n","")
+		# ^ 
 	end
 	 puts "Les crypto s'appelle : #{prices[0..10]}"
 	 #sleep(5)
