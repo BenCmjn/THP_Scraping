87f1925e967159e97d62c9b971488bb23d2af950
BenCmjn <benjamin.calmejane@gmail.com>
2018-01-19 00:18:59 +0100

    ptite idée avant d'aller dormir

---
+++ THP_ascii_art.rb
@@ -0,0 +1,30 @@
+#------------------------------------------
+# =>  Le projet : 
+#------------------------------------------
+# - choisir un mot 
+# - prendre un style ascii-art
+# (http://www.patorjk.com/software/taag/#p=display&f=Graffiti&t=Type%20Something%20)
+# (https://www.wikiwand.com/en/ASCII_art)
+
+# - le coller ici avec le style ascii
+# - remplacer des caractères répétitifs par des emojis THP (:parrotnyan: par exemple)  
+# - pouvoir coller le mot transformé sur Slack
+# - partager sa création
+# - avoir la class deNerdRicainCommeDansLesFilms
+
+## Le truc à placer (par exemple) :parrotnyan:
+## À la place de "$" (par exemple)
+## Sur quelque chose comme :
+
+#  /$$$$$$$$ /$$   /$$ /$$$$$$$ 
+# |__  $$__/| $$  | $$| $$__  $$
+#    | $$   | $$  | $$| $$  \ $$
+#    | $$   | $$$$$$$$| $$$$$$$/
+#    | $$   | $$__  $$| $$____/ 
+#    | $$   | $$  | $$| $$      
+#    | $$   | $$  | $$| $$      
+#    |__/   |__/  |__/|__/      
+                           
+# http://www.patorjk.com/software/taag/#p=display&h=0&f=Big%20Money-ne&t=THP
+
+
